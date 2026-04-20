265f58e2af7cde62f5c7374f2bd0ab466dfacf5f
LEGCIUS <danielrb4567@gmail.com>
2026-04-19 19:45:06 -0600

    Guia terminada

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,9 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { updateInvoice, State } from '@/app/lib/actions';
+import { useActionState } from 'react';
+
 
 export default function EditInvoiceForm({
   invoice,
@@ -16,9 +19,15 @@ export default function EditInvoiceForm({
 }: {
   invoice: InvoiceForm;
   customers: CustomerField[];
+
 }) {
+
+  const initialState: State = { message: null, errors: {} };
+  const [state, formAction] = useActionState(updateInvoice.bind(null, invoice.id), initialState);
+
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   return (
-    <form>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -120,4 +129,4 @@ export default function EditInvoiceForm({
       </div>
     </form>
   );
-}
+  }
